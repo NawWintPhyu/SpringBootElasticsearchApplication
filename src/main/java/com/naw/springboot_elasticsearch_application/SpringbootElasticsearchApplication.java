d5fb738de5378dbcbb9a6dc307a5de15a8ddd944
NawWintPhyu <wintphyu2013@gmail.com>
2024-09-28 09:53:18 +0630

    commit inital project sample

---
+++ src/main/java/com/naw/springboot_elasticsearch_application/SpringbootElasticsearchApplication.java
@@ -0,0 +1,17 @@
+package com.naw.springboot_elasticsearch_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+Before run this application, need to run Elasticserch via terminal.
+Use elasticsearch-8.3.2
+ */
+@SpringBootApplication
+public class SpringbootElasticsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootElasticsearchApplication.class, args);
+    }
+
+}
